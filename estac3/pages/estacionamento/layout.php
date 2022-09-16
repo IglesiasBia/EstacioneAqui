44c0b5b868b7b69eba4ed6313a3c666dd6af6634
IglesiasBia <beatriziglesias.santos.estudante@eteot.rio.br>
2022-09-16 00:52:45 -0300

    Desenhando layout

--- estac3/pages/estacionamento/layout.php
+++ estac3/pages/estacionamento/layout.php
@@ -1,32 +1,93 @@
 <?php
-$numLinhas = 2;
-$num = 0;
-$ultimaVagaLinhas = array();
-while($num < $numLinhas){
-    $ultimavaga[$num] = $_POST["ultimaVaga"[$num]];   
-    array_push($ultimaVagaLinhas, $ultimavaga[$num]);
-    $num++;
-}
-echo $ultimaVagaLinhas;
-
-// $array = [];
+// $numLinhas = 2;
+// $num = 0;
+// $ultimaVagaLinhas = array();
 // while($num < $numLinhas){
-//     echo "<tr>"; 
-//     // while(){}
+//     $ultimavaga[$num] = $_POST["ultimaVaga"[$num]];   
+//     array_push($ultimaVagaLinhas, $ultimavaga[$num]);
+//     $num++;
 // }
-// $inicioPrimeiraLinha = 1;
-// $finalPrimeiraLinha = 5;
+// echo $ultimaVagaLinhas;
 
-// $inicioSegundaLinha = 6;
-// $finalSegundaLinha = 10;
+// // $array = [];
+// // while($num < $numLinhas){
+// //     echo "<tr>"; 
+// //     // while(){}
+// // }
+// // $inicioPrimeiraLinha = 1;
+// // $finalPrimeiraLinha = 5;
 
+// // $inicioSegundaLinha = 6;
+// // $finalSegundaLinha = 10;
+// $vagas = 100;
+// $contador = 1;
 ?>
 
 
+
+
 <table>
-    
 <tr>
-        <td>oi</td>
-        <td>oi</td>
-    </tr>
+    <div id="teste"> </div>
+<script>
+    let vagas = 234;
+    let contador = 1;
+    
+    while(contador <= vagas){
+        // console.log(screen.width);
+
+        // let main = document.getElementsByClassName("navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl");
+        // console.log(main);
+        let elemento = document.getElementById("teste");
+        elemento.innerHTML+=`<td><button type='button' class='btn btn-success btnvaga' onclick="apagaVaga()" id='vaga`+contador+`' style="position: absolut">
+        <img src='../../assets/img/icons/carlayout.png' width='30em'alt=''> 
+        </button></td>`;
+        // echo "<td><button type='button' class='btn btn-success btnvaga'>
+        // <img src='../../assets/img/icons/carlayout.png' width='30em'alt=''> 
+        // </button></td>";
+
+        contador++;
+
+     }
+
+     function apagaVaga(){
+        let el = event.target || event.srcElement;
+        let id = el.id;
+        console.log(id);
+        let botao = document.getElementById(id);
+        let classe = botao.className;
+        console.log(classe);
+        if(classe.includes('btn-success')){
+            botao.classList.remove('btn-success');
+            // botao.classList.add('--bs-gray-700');
+            
+            botao.style.backgroundColor = "gray";
+        }else{
+            botao.classList.add('btn-success');
+            botao.style.backgroundColor = "#4CAF50";
+        }
+        console.log(classe);
+       
+        // botao.style.display = "none";
+     }
+   
+</script>
+
+
+
+    <?php
+    //  while($contador <= $vagas){
+
+    //     echo "<td><button type='button' class='btn btn-success btnvaga'>
+    //     <img src='../../assets/img/icons/carlayout.png' width='30em'alt=''> 
+    //     </button></td>";
+
+
+
+    //     if($contador)
+    //     $contador++;
+    //  }
+   
+    ?>
+</tr>
 </table>
